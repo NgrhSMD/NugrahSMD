07ef7a84c1ec88b1ad2fc38da354cedfe80fd646
NgrhSMD <75657398+NgrhSMD@users.noreply.github.com>
2021-02-11 20:05:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,21 +18,21 @@ const apivhtear = 'YOUR_APIKEY'; // APIKEY VHTEAR
 const apibarbar = 'YOUR_APIKEY'; // APIKEY MHANKBARBAR
 const tobzkey = 'YOUR_APIKEY'; // APIKEY TOBZ
 const vinzkey = 'YOUR_APIKEY'; // APIKEY ZEKS
-const BotName = 'SADBOTZ'; // GANTI PAKE NAMA BOT LU
-const wa = 'https://chat.whatsapp.com/DcSr2e5hLMd7PkufBVymgu'; // LINK GRUP WA JANGAN DIGANTI YA BIAR KUMPUL DISITU SALING MENGENAL:)
-const ow = 'RAMLAN'; // GANTI PAKE NAMA MU
-const nomorowner = '085559240360'; // GANTI PAKE NOMOR MU ( JANGAN NOMOR BOT )
-const ovo = '0855-5924-0360'; // GANTI PAKE NOMOR OVO LU
-const pulsa = '0812-1444-1027'; // GANTI PAKE NOMOR PULSA LU
-const dana = '0855-5924-0360'; // GANTI PAKE NOMOR DANA LU
-const gopay = '0855-5924-0360'; // GANTI PAKE NOMOR GOPAY LU
+const BotName = 'NGRHBOT'; // GANTI PAKE NAMA BOT LU
+const wa = 'https://chat.whatsapp.com/CWAuPehYPZPAFslGN2by79'; // LINK GRUP WA JANGAN DIGANTI YA BIAR KUMPUL DISITU SALING MENGENAL:)
+const ow = 'Nugrah'; // GANTI PAKE NAMA MU
+const nomorowner = '081257489859'; // GANTI PAKE NOMOR MU ( JANGAN NOMOR BOT )
+const ovo = '0812-5748-9859'; // GANTI PAKE NOMOR OVO LU
+const pulsa = '0812-5748-9859'; // GANTI PAKE NOMOR PULSA LU
+const dana = '0812-5748-9859'; // GANTI PAKE NOMOR DANA LU
+const gopay = '0812-5748-9859'; // GANTI PAKE NOMOR GOPAY LU
 const instagram = 'http://www.instagram.com/iamramlan_'; // GANTI IG LU
-const aktif = '08:00 - 22:00'; // TERSERAH LU DAH INI
+const aktif = '00:00 - 00:00'; // TERSERAH LU DAH INI
 const vcard = 'BEGIN:VCARD\n'
   + 'VERSION:3.0\n' // MAU NGAPAIN TOD
-  + 'FN:RAMLAN GANS\n' // GANTI PAKE NAMA MU
+  + 'FN:NugrahSMD\n' // GANTI PAKE NAMA MU
   + 'ORG:OWNER BOT;\n' // INI BIARIN GPP, GANTI PAKE NAMA BOT BOLEH
-  + 'TEL;type=CELL;type=VOICE;waid=6285559240360:+62 855-5924-0360\n' // GANTI PAKE NOMOR LU ( SAMAIN KEK GITU YA )
+  + 'TEL;type=CELL;type=VOICE;waid=6281257489859:+62 812-5748-9859\n' // GANTI PAKE NOMOR LU ( SAMAIN KEK GITU YA )
   + 'END:VCARD' // THANKS TO LANN ID
 // NOTE : UBAH YANG GUE SURUH UBAH AJA BIAR GAK ERROR
 const
@@ -64,7 +64,7 @@ conn.on('qr', qr => {
     {
       small: true
     });
-  console.log(`[ ${moment().format("HH:mm:ss")} ] SUBSCRIBE YT RAMLAN CHANNEL!`);
+  console.log(`[ ${moment().format("HH:mm:ss")} ] FOLLOW IG :V @nugrahsemed!`);
 });
 
 conn.on('credentials-updated', () => {
@@ -374,7 +374,7 @@ Note : Jangan males membaca !!!
          ────────────────
 Note : Sisanya gunain otak kalian aja cape w jelasin :)
          ────────────────
-🔰 -----[ *POWERED BY RAMLAN ID* ]----- 🔰`,
+🔰 -----[ *POWERED BY NGRHSMD* ]----- 🔰`,
       contextInfo: { mentionedJid: [nomor] }
     }
     conn.sendMessage(id, options, MessageType.text, { quoted: m })
@@ -407,7 +407,7 @@ Note : Sisanya gunain otak kalian aja cape w jelasin :)
 ║➣ *HARAP HUBUNGI NOMOR DIBAWAH :*
 ║➣ *☎️ WA : *085559240360*
 
-🔰 -----[ *POWERED BY LANN ID* ]----- 🔰`,
+🔰 -----[ *POWERED BY NGRHSMD* ]----- 🔰`,
       contextInfo: { mentionedJid: [nomor] }
     }
     conn.sendMessage(id, options, MessageType.text, { quoted: m })
@@ -566,7 +566,7 @@ var teks = text.replace(/!ssweb /, '')
   if (text.includes('!tagme')) {
     var nomor = m.participant
     const options = {
-      text: `@${nomor.split("@s.whatsapp.net")[0]} Hallo sayang... 🤗`,
+      text: `@${nomor.split("@s.whatsapp.net")[0]} P... 🤗`,
       contextInfo: { mentionedJid: [nomor] }
     }
     conn.sendMessage(id, options, MessageType.text)
